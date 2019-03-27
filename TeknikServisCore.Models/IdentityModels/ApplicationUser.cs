a89463dd158642a6c12ba9fd489d32260525fc69
CrephonteS <aykutkaragoz94@gmail.com>
2019-03-27 15:50:59 +0300

    Bundleconfig ve UserLayout oluşturuldu.

--- TeknikServisCore.Models/IdentityModels/ApplicationUser.cs
+++ TeknikServisCore.Models/IdentityModels/ApplicationUser.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace TeknikServisCore.Models.IdentityModels
 {
+   
     public class ApplicationUser:IdentityUser
     {
+ 
         public string Name { get; set; }
         public string Surname { get; set; }
         public DateTime BirthDate { get; set; }
